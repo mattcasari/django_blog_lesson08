cc7a477d176b586e75aea731a07363d05ff6e695
matteoVT <mjcasari@gmail.com>
2021-02-08 20:14:21 -0800

    Initial commit

--- mysite/polling/tests.py
+++ mysite/polling/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from polling.models import Post  # add this import at the top
+
+class PostTestCase(TestCase):
+    # def setUp(self):
+    #     self.user = User.objects.get(pk=1)
+
+    # add this test method to the PostTestCase
+    def test_string_representation(self):
+        # Given: a title
+        expected = "This is a title"
+
+        # When: the post is created
+        p1 = Post(title=expected)
+        actual = str(p1)
+
+        # Then: The class __str__ should be the title
+        self.assertEqual(expected, actual)
+
