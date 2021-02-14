3a55adf35c962a4b6ce04c553c637146f0a28a13
matteoVT <mjcasari@gmail.com>
2021-02-13 22:28:44 -0800

    Adding REST framework.
    REST framework up and running

--- blogging/views.py
+++ blogging/views.py
@@ -4,6 +4,46 @@ from django.template import loader
 
 from blogging.models import Post
 
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from blogging.serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 # def stub_view(request, *args, **kwargs):
 #     body = "StubView\n\n"
 #     if args:
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,38 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from django.contrib.auth.views import LoginView, LogoutView
+from rest_framework import routers
+from blogging import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'categories', views.CategoryViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout")
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+# urlpatterns = [
+#     path('', include('blogging.urls')),
+#     path('polling/', include('polling.urls')),
+#     path('admin/', admin.site.urls),
+#     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+#     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+# ]
